e1e6ed68a34e95c79e139a072cdfd61748215d2f
Christian Teijon (crunis) <christian.teijon@rightscale.com>
2017-08-08 20:00:25 +0200

    Add option to ignore APT pkg authentication

--- travis-setup.sh
+++ travis-setup.sh
@@ -12,13 +12,20 @@ docker_install()
     export APT_DOCKER_PKG=$DEFAULT_APT_DOCKER_PKG
   fi
 
-  echo "*** Adding dockerproject key"
-  sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys F76221572C52609D
+  if [[ $ALLOW_UNAUTHENTICATED == "true" ]]
+  then
+    echo "WARNING!!!! Installing unauthenticated packages !!!!"
+    export APT_PARAMS="--allow-unathenticated"
+  else
+    echo "*** Adding dockerproject key"
+    sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys F76221572C52609D
+    export APT_PARAMS=""
+  fi
   echo "*** Adding apt.dockerproject.org repository (trusty)"
   echo 'deb "https://apt.dockerproject.org/repo" ubuntu-trusty main' | sudo tee /etc/apt/sources.list.d/docker.list
   sudo apt-get update
   echo "*** Installing $APT_DOCKER_PKG"
-  sudo apt-get -y --allow-downgrades -o Dpkg::Options::="--force-confnew" install $APT_DOCKER_PKG && docker -v
+  sudo apt-get -y --allow-downgrades $APT_PARAMS -o Dpkg::Options::="--force-confnew" install $APT_DOCKER_PKG && docker -v
 }
 
 check_sudo()
