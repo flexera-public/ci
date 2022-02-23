8df5cdfda139a1efeb3641c4834eeea042cb9dba
HarishKMedaal <92308477+HarishKMedaal@users.noreply.github.com>
2022-02-23 17:39:08 +0530

    updated url of releases

--- travis-setup.sh
+++ travis-setup.sh
@@ -19,7 +19,7 @@ docker_install()
     export APT_PARAMS=""
   fi
   echo "*** Adding apt.dockerproject.org repository (trusty)"
-  echo 'deb "https://apt.dockerproject.org/repo" ubuntu-trusty main' | sudo tee /etc/apt/sources.list.d/docker.list
+  echo 'deb "https://download.docker.com/linux/ubuntu/dists trusty main' | sudo tee /etc/apt/sources.list.d/docker.list
   sudo apt-get update
   echo "*** Installing $APT_DOCKER_PKG"
   sudo apt-get -y --allow-downgrades $APT_PARAMS -o Dpkg::Options::="--force-confnew" install $APT_DOCKER_PKG && docker -v
