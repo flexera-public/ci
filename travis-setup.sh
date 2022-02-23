b54b6e27b5dbee3981f782c6a0f081af67215308
HarishKMedaal <92308477+HarishKMedaal@users.noreply.github.com>
2022-02-23 18:11:43 +0530

    corrections needed

--- travis-setup.sh
+++ travis-setup.sh
@@ -21,7 +21,7 @@ docker_install()
   echo "*** Adding download.docker.com repository (focal)"
   sudo apt-get update
   sudo apt-get install ca-certificates curl gnupg lsb-release
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o -y /usr/share/keyrings/docker-archive-keyring.gpg
   echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update
   echo "*** Installing $APT_DOCKER_PKG"
